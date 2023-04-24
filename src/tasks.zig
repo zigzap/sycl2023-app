fa22a279c7691c46e8d1690aa9c3d6bcb9ef5f30
Rene Schallner <rene@renerocks.ai>
2023-04-24 17:15:53 +0200

    fix

--- src/tasks.zig
+++ src/tasks.zig
@@ -3,6 +3,7 @@ json_template: std.json.ValueTree = undefined,
 
 const Self = @This();
 
+const dummy_data_json = @embedFile("data/sycl2023-survey.json");
 
 pub fn init(a: std.mem.Allocator) !Self {
     var parser = std.json.Parser.init(a, false);
