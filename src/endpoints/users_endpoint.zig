3cc5475bd5d10f675a6c417797ca0f92f9dfe7ca
Rene Schallner <rene@renerocks.ai>
2023-05-04 14:47:27 +0200

    better split /users endpoint into /list and /save

--- src/endpoints/users_endpoint.zig
+++ src/endpoints/users_endpoint.zig
@@ -16,7 +16,7 @@ pub fn init(a: std.mem.Allocator, users_path: []const u8, users: *Users) !Self {
         .users = users,
         .endpoint = zap.SimpleEndpoint.init(.{
             .path = users_path,
-            .get = listUsers,
+            .get = listOrSaveUsers,
             .post = null,
             .put = null,
             .delete = null,
@@ -35,49 +35,41 @@ fn unauthorized(e: *zap.SimpleEndpoint, r: zap.SimpleRequest) void {
     std.debug.print("UNAUTHORIZED\n", .{});
 }
 
-// shouldn't we just return which user is in which task?
-fn listUsers(e: *zap.SimpleEndpoint, r: zap.SimpleRequest) void {
+fn listOrSaveUsers(e: *zap.SimpleEndpoint, r: zap.SimpleRequest) void {
     const self = @fieldParentPtr(Self, "endpoint", e);
 
-    // if 0 users, reply instantly
-    if (self.users.current_user_id == 0) {
-        r.sendJson("[]") catch return;
-        return;
-    }
-
-    var do_save = false;
-
-    if (r.query) |q| {
-        if (std.mem.startsWith(u8, q, "save")) {
-            do_save = true;
-        } else {
-            std.debug.print("    unknown query: {s}\n", .{q});
-        }
-    }
-    var buf = self.allocator.alloc(
-        u8,
-        self.users.current_user_id * 512 * 1024, // 512kb per user
-    ) catch return;
-    defer self.allocator.free(buf);
-
-    std.debug.print("    Allocated buffer size: {d}kB\n", .{buf.len / 1024});
-    var fba = std.heap.FixedBufferAllocator.init(buf);
-    var string = std.ArrayList(u8).init(fba.allocator());
-    if (self.users.jsonStringify(.{}, string.writer())) {
-        if (do_save) {
-            std.debug.print("    Saving to users.json...  ", .{});
-            if (self.saveUsers(string.items)) {
-                std.debug.print("DONE!\n", .{});
+    if (r.path) |path| {
+        if (std.mem.endsWith(u8, path, "/save")) {
+            if (self.usersToJsonAlloc()) |allocJson| {
+                std.debug.print("    Saving to users.json...  ", .{});
+                if (self.saveUsers(allocJson.json)) {
+                    self.allocator.free(allocJson.buffer_to_free);
+                    std.debug.print("DONE!\n", .{});
+                    r.sendJson("{ \"status\": \"OK\"}") catch return;
+                } else |err| {
+                    std.debug.print("ERROR {any}!\n", .{err});
+                    r.sendJson("{ \"error\": \"not saved\"}") catch return;
+                }
             } else |err| {
-                std.debug.print("ERROR {any}!\n", .{err});
+                // TODO: what's the best status to return?
+                std.debug.print("    /users/save error: {any}\n", .{err});
+                r.setStatus(.not_found);
+                r.sendJson("{ \"status\": \"not found\"}") catch return;
+            }
+        } else if (std.mem.endsWith(u8, path, "/list")) {
+            if (self.usersToJsonAlloc()) |allocJson| {
+                defer self.allocator.free(allocJson.buffer_to_free);
+                r.sendJson(allocJson.json) catch return;
+            } else |err| {
+                // TODO: what's the best status to return?
+                std.debug.print("    /users/list error: {any}\n", .{err});
+                r.setStatus(.not_found);
+                r.sendJson("{ \"status\": \"not found\"}") catch return;
             }
         }
-        r.sendJson(string.items) catch return;
-    } else |err| {
-        std.debug.print("    /users LIST Error: {any}\n", .{err});
-        r.setStatus(.not_found);
-        r.sendJson("{ \"status\": \"not found\"}") catch return;
     }
+    r.setStatus(.not_found);
+    r.sendJson("{ \"status\": \"no path\"}") catch return;
 }
 
 fn saveUsers(self: *Self, json: []const u8) !void {
@@ -90,4 +82,29 @@ fn saveUsers(self: *Self, json: []const u8) !void {
     try writer.writeAll(json);
     try buffered_writer.flush();
     f.close();
+}
+
+const AllocJson = struct {
+    json: []const u8,
+    buffer_to_free: []u8,
+};
+
+fn usersToJsonAlloc(self: *Self) !AllocJson {
+    var buf = try self.allocator.alloc(
+        u8,
+        self.users.current_user_id * 512 * 1024, // 512kb per user
+    );
+    errdefer self.allocator.free(buf);
+
+    std.debug.print("    Allocated buffer size: {d}kB for {d} users.\n", .{ buf.len / 1024, self.users.current_user_id });
+    var fba = std.heap.FixedBufferAllocator.init(buf);
+    var string = std.ArrayList(u8).init(fba.allocator());
+    if (self.users.jsonStringify(.{}, string.writer())) {
+        return .{
+            .json = string.items,
+            .buffer_to_free = buf,
+        };
+    } else |err| {
+        return err;
+    }
 }
